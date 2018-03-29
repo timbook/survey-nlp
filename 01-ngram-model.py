df9299cb865919ce9e2663d2a313e9843bb54a8c
TimothyKBook <TimothyKBook@gmail.com>
2018-03-29 10:39:31 -0400

    added ngram model outputter

---
+++ 01-ngram-model.py
@@ -0,0 +1,44 @@
+import sys, os
+import nltk
+import pandas as pd
+import numpy as np
+
+import ngram
+
+k_vec = [int(i) for i in sys.argv[1:]]
+
+comps_df = pd.read_csv('output/complaint-topics.csv')
+comps = comps_df['Consumer complaint narrative']
+
+for k in k_vec:
+    print("Entering loop %d" % k)
+    comps_k = list(comps[comps_df.topic11 == k])
+    print("There are %d rows in complaint %d" % (len(comps_k), k))
+
+    # Break apart complaints that are multiple sentences long.
+    comps_k = [
+        sent 
+        for sents in comps_k
+        for sent in nltk.tokenize.sent_tokenize(sents)
+    ]
+
+    # Instantiate trigram model.
+    mod = ngram.NGramModel(comps_k, 3)
+
+    # Filter complaints into a format efficiently used in trigram.
+    mod.filterSents([
+        ('(19|20)[0-9]{2}', 'YEAR'),
+        ('[^A-Za-z0-9 .!?]', ''),
+        ('[Xx]{2,}', 'PROPER_NOUN'),
+        ('[\.]{3,}', '___')
+    ])
+
+    # Initiate N-grams with k = 0.05 (for now)
+    mod.makeNGrams(0.05)
+
+    # Write random sentences to a text file.
+    fname = 'random-sents/t%02d-sents.txt' % k
+    with open(fname, 'w') as f:
+        for i in range(10):
+            print('Printing sentence number %d' % i)
+            f.write(mod.genSent() + '\n')
